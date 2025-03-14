0380e89bedd6b2d39ef9ee58bb2e10de59f89d22
Samrat Baral <sbaral30064@ucumberlands.edu>
2025-03-14 19:39:51 -0400

    Create lab5.sh

---
+++ src/lab5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python -m venv venv
+fi
+
+echo "Activating virtual environment..."
+if [ -f "venv/Scripts/activate" ]; then
+    source venv/Scripts/activate
+else
+    source venv/bin/activate
+fi
+
+echo "Upgrading pip and installing required packages..."
+pip install --upgrade pip
+pip install pyshark requests
+
+echo "Running lab5.py..."
+python lab5.py
+
+echo "Deactivating virtual environment..."
+deactivate
